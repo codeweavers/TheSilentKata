af60811d0db155fe3af56e9e4819594de93f9fe8
PaulBoocock <paulboocock@codeweavers.net>
2017-05-17 11:12:17 +0100

    Initial Commit of C# Starter project

---
+++ CSharp/TheSilentKata.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace TheSilentKata.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Fail()
+        {
+            Assert.Fail("FIX ME");
+        }
+    }
+}
